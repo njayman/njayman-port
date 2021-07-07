336f6621ecc76cd9e741d2bd2fd2176797433366
njayman <najishm828282@gmail.com>
2021-07-07 12:10:39 +0600

    initial setup and infos

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import "./style.css";
+
+let information = [{ title: "a" }, { title: "b" }, { title: "c" }];
+let idx = 0;
+let changableSection = document.getElementById("changable")!;
+const changeChangableSection = (step: string) => {
+  if (step === "forward") {
+    ++idx;
+    if (idx >= information.length) {
+      idx = 0;
+    }
+  } else if (step === "backward") {
+    --idx;
+    if (idx < 0) {
+      idx = information.length - 1;
+    }
+  }
+  console.log(idx);
+  changableSection.innerHTML = `<h1>${information[idx].title}</h1>`;
+};
+
+changeChangableSection("");
+
+// setInterval(() => {
+//   changeChangableSection("forward");
+// }, 2000);
